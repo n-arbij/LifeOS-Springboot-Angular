3d5cfe6620efd6e5f28773f589ff1af8df57c0a5
n-arbij <omollojibran@gmail.com>
2026-07-01 16:16:19 +0300

    upt: Sidebar & Authenticated Layout redesign

--- frontend/src/app/layout/authenticated/authenticated-layout/authenticated-layout.component.ts
+++ frontend/src/app/layout/authenticated/authenticated-layout/authenticated-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from '../../../components/sidebar/sidebar';
 import { RouterOutlet } from '@angular/router';
+import { Header } from '../../../components/header/header';
 
 @Component({
   selector: 'app-authenticated-layout',
-  imports: [Sidebar, RouterOutlet],
+  imports: [Sidebar, RouterOutlet, Header],
   templateUrl: './authenticated-layout.component.html',
   styleUrl: './authenticated-layout.component.css',
 })
