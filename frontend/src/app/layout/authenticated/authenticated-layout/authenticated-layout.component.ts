7dc4d89753f1ea5a5b1265f257f3021eb99d35df
n-arbij <omollojibran@gmail.com>
2026-07-01 11:41:53 +0300

    feat: Dashboard Sidebar

--- frontend/src/app/layout/authenticated/authenticated-layout/authenticated-layout.component.ts
+++ frontend/src/app/layout/authenticated/authenticated-layout/authenticated-layout.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Sidebar } from '../../../components/sidebar/sidebar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-authenticated-layout',
-  imports: [],
+  imports: [Sidebar, RouterOutlet],
   templateUrl: './authenticated-layout.component.html',
   styleUrl: './authenticated-layout.component.css',
 })
